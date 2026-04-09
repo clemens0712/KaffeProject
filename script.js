c6bea986fd5ffcbf9372d2c6998065f4385bd27a
arnold <cle.pojar@hakvb.at>
2026-04-09 12:05:16 +0200

    Revert "Falsche Werbeaktion"
    This reverts commit 06e772f905d6753c84b7ec9ea358cdea6de8969a.

--- script.js
+++ script.js
@@ -2,5 +2,4 @@ function begruessungAnzeigen() {
 const ausgabe = document.getElementById("ausgabe"); 
 ausgabe.textContent = "Heute gibt es frischen Apfelstrudel und Cappuccino."; 
 <script src="script.js"></script> 
-console.log("Testmodus aktiviert");
 } 
