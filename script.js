06e772f905d6753c84b7ec9ea358cdea6de8969a
arnold <cle.pojar@hakvb.at>
2026-04-09 12:04:09 +0200

    Falsche Werbeaktion

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function begruessungAnzeigen() {
 const ausgabe = document.getElementById("ausgabe"); 
 ausgabe.textContent = "Heute gibt es frischen Apfelstrudel und Cappuccino."; 
 <script src="script.js"></script> 
+console.log("Testmodus aktiviert");
 } 
